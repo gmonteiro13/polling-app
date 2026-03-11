1b5a4ecdd22f7dfe700f18de15b0b88b2ebe0563
gmonteiro13 <gscmonteiro@gmail.com>
2026-03-11 15:13:44 -0300

    Adiciona testes para postagem de pergunta futura (não deve aparecer nos posts), pergunta no passado e futuro não pode ser recente e troca views para classes

--- polls/urls.py
+++ polls/urls.py
@@ -4,12 +4,10 @@ from . import views
 
 app_name = "polls"
 urlpatterns = [
-    # ex: /polls/
-    path("", views.index, name="index"),
-    # ex: /polls/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /polls/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /polls/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    # abaixo, troca-se o question_id por pk, que é o nome padrão para a chave primária.
+    # o DetailView espera que a chave primária seja chamada pk ou id, então usando pk, não precisamos alterar o código do DetailView
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
